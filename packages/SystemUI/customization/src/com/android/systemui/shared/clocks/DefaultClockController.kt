cdad744bae5f545c7917526323af895e69c37394
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-26 13:20:19 +0800

    SystemUI: Resolve crashes caused by allowing the reinflation of SystemUI.
    Test: Perform extensive dark/light theme toggling.
    
    Change-Id: I0c7180f3ca19b4e6a122d4b4f30d14f67f20d384
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- packages/SystemUI/customization/src/com/android/systemui/shared/clocks/DefaultClockController.kt
+++ packages/SystemUI/customization/src/com/android/systemui/shared/clocks/DefaultClockController.kt
@@ -180,16 +180,18 @@ class DefaultClockController(
         }
 
         override fun recomputePadding(targetRegion: Rect?) {
-            // We center the view within the targetRegion instead of within the parent
-            // view by computing the difference and adding that to the padding.
-            val parent = view.parent
-            val yDiff =
-                if (targetRegion != null && parent is View && parent.isLaidOut())
-                    targetRegion.centerY() - parent.height / 2f
-                else 0f
-            val lp = view.getLayoutParams() as FrameLayout.LayoutParams
-            lp.topMargin = (-0.5f * view.bottom + yDiff).toInt()
-            view.setLayoutParams(lp)
+            if (view != null) {
+                // We center the view within the targetRegion instead of within the parent
+                // view by computing the difference and adding that to the padding.
+                val parent = view.parent
+                val yDiff =
+                    if (targetRegion != null && parent is View && parent.isLaidOut())
+                        targetRegion.centerY() - parent.height / 2f
+                    else 0f
+                val lp = view.getLayoutParams() as FrameLayout.LayoutParams
+                lp.topMargin = (-0.5f * view.bottom + yDiff).toInt()
+                view.setLayoutParams(lp)
+            }
         }
 
         /** See documentation at [AnimatableClockView.offsetGlyphsForStepClockAnimation]. */

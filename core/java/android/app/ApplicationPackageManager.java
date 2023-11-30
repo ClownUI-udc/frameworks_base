CherishOS/android_frameworks_base

PixelPropsUtils: Refactor
* Allows some devices to fully pass safetynet's strong attestation (maybe due to prebuilt vendor)
* Do not spoof restore, pixelmigrate and setupwizard
* Enable Photos spoof by default
* Keep Photos happy and not count towards storage quota
* Separate GamesProps into GamesPropsUtils
* Switch from POCO F4 to POCO F5
* Transfer GMS spoofing from AttestationHooks

Co-authored-by: someone5678 <nemui3353@gmail.com>
Co-authored-by: aswin7469 <aswinas@pixysos.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: Gustavo Mendes <gusttavo.me@outlook.com>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

core/java/android/app/ApplicationPackageManager.java
@@ -837,7 +837,7 @@ public class ApplicationPackageManager extends PackageManager {
             "com.google.android.feature.LILY_EXPERIENCE",
             "com.google.android.feature.TURBO_PRELOAD",
             "com.google.android.feature.WELLBEING",
-            "com.google.lens.feature.IMAGE_INTEGRATION",    
+            "com.google.lens.feature.IMAGE_INTEGRATION",
             "com.google.lens.feature.CAMERA_INTEGRATION",
             "com.google.photos.trust_debug_certs",
             "com.google.android.feature.AER_OPTIMIZED",
@@ -848,22 +848,22 @@ public class ApplicationPackageManager extends PackageManager {
             "com.google.android.apps.dialer.SUPPORTED"
     };
 
-    private static final String[] featuresP23 = {
+    private static final String[] featuresTensor = {
             "com.google.android.feature.PIXEL_2024_EXPERIENCE",
             "com.google.android.feature.PIXEL_2024_MIDYEAR_EXPERIENCE",
             "com.google.android.feature.PIXEL_2023_EXPERIENCE",
             "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE",
-    };
-
-    private static final String[] featuresTensor = {
             "com.google.android.feature.PIXEL_2022_EXPERIENCE",
             "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2021_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_EXPERIENCE"
     };
 
     private static final String[] featuresNexus = {
             "com.google.android.apps.photos.NEXUS_PRELOAD",
             "com.google.android.apps.photos.nexus_preload",
+            "com.google.android.feature.PIXEL_EXPERIENCE",
+            "com.google.android.feature.GOOGLE_BUILD",
+            "com.google.android.feature.GOOGLE_EXPERIENCE"
     };
 
     private static final String[] featuresAndroid = {
@@ -878,11 +878,10 @@ public class ApplicationPackageManager extends PackageManager {
         }
         String packageName = ActivityThread.currentPackageName();
         if (packageName != null &&
-                (packageName.equals("com.google.android.apps.photos")
-                && SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true))) {
+                packageName.equals("com.google.android.apps.photos") &&
+                SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
             if (Arrays.asList(featuresPixel).contains(name)) return false;
             if (Arrays.asList(featuresPixelOthers).contains(name)) return true;
-            if (Arrays.asList(featuresP23).contains(name)) return false;
             if (Arrays.asList(featuresNexus).contains(name)) return true;
         }
         if (Arrays.asList(featuresAndroid).contains(name)) return true;

ClownUI-udc/frameworks_base

Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

core/java/android/app/ApplicationPackageManager.java
@@ -856,14 +856,11 @@ public class ApplicationPackageManager extends PackageManager {
             "com.google.android.apps.dialer.SUPPORTED"
     };
 
-    private static final String[] featuresP23 = {
+    private static final String[] featuresTensor = {
             "com.google.android.feature.PIXEL_2024_EXPERIENCE",
             "com.google.android.feature.PIXEL_2024_MIDYEAR_EXPERIENCE",
             "com.google.android.feature.PIXEL_2023_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE"
-    };
-
-    private static final String[] featuresTensor = {
+            "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE",
             "com.google.android.feature.PIXEL_2022_EXPERIENCE",
             "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
             "com.google.android.feature.PIXEL_2021_EXPERIENCE"
@@ -890,10 +887,9 @@ public class ApplicationPackageManager extends PackageManager {
         String packageName = ActivityThread.currentPackageName();
         if (packageName != null &&
                 packageName.equals("com.google.android.apps.photos") &&
-                SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", false)) {
+                SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
             if (Arrays.asList(featuresPixel).contains(name)) return false;
             if (Arrays.asList(featuresPixelOthers).contains(name)) return true;
-            if (Arrays.asList(featuresP23).contains(name)) return false;
             if (Arrays.asList(featuresNexus).contains(name)) return true;
         }
         if (Arrays.asList(featuresAndroid).contains(name)) return true;